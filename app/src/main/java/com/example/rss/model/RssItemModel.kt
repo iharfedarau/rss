742aff8fb97b239bd78ae68bffbdb038dcdce119
iharfedarau <ihar.fedarau@gmail.com>
2021-11-12 12:49:31 +0300

    Add swiperefreshlayout

--- app/src/main/java/com/example/rss/model/RssItemModel.kt
+++ app/src/main/java/com/example/rss/model/RssItemModel.kt
@@ -5,17 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.rss.model.RetrofitInstance
-import com.example.rss.model.RssModelItem
-import com.example.rss.model.RssModelRootItem
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
 
 class RssItemModel: ViewModel() {
     private var rssItems = MutableLiveData<List<RssModelItem>>()
@@ -24,10 +17,10 @@ class RssItemModel: ViewModel() {
         get() = rssItems
 
     init {
-        fetchData()
+        fetchData{}
     }
 
-    private fun fetchData() {
+    private fun fetchData(onRefresFinished: suspend () -> Unit) {
         viewModelScope.launch(Dispatchers.IO) {
             val response  = try {
                 RetrofitInstance.api.getRssModelRootItem()
@@ -37,14 +30,15 @@ class RssItemModel: ViewModel() {
                 Log.e("retrofit", "HttpException")
             }
 
-            if (response != null) {
-                rssItems.postValue((response as RssModelRootItem).rssModelItemList)
+            launch (Dispatchers.Main){
+                rssItems.value = (response as RssModelRootItem).rssModelItemList
+                onRefresFinished()
             }
         }
     }
 
-    public fun refresh() {
+    public fun refresh(onRefresFinished: suspend () -> Unit) {
         rssItems.value = listOf<RssModelItem>()
-        fetchData()
+        fetchData(onRefresFinished)
     }
 }
--- app/src/main/java/com/example/rss/presentation/RssFragment.kt
+++ app/src/main/java/com/example/rss/presentation/RssFragment.kt
@@ -2,23 +2,22 @@ package com.example.rss.presentation
 
 import android.os.Bundle
 import android.view.*
-import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.rss.R
-import com.example.rss.databinding.FragmentDetailsBinding
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
 import com.example.rss.databinding.FragmentRssBinding
 import com.example.rss.model.RssItemAdapter
 import com.example.rss.model.RssItemModel
+
 
 class RssFragment : Fragment() {
     private lateinit var viewModel: RssItemModel
     private  lateinit var binding: FragmentRssBinding
 
-    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRssBinding.inflate(inflater, container, false)
         return binding.root
@@ -28,12 +27,25 @@ class RssFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.refreshRssButton.setOnClickListener {
-            viewModel.refresh()
+            binding.swipeContainer.setRefreshing(true);
+            viewModel.refresh{
+                binding.swipeContainer.setRefreshing(false);
+            }
+        }
+
+        binding.swipeContainer.setOnRefreshListener{
+            viewModel.refresh{
+                binding.swipeContainer.setRefreshing(false);
+            }
         }
 
         viewModel = ViewModelProvider(this).get(RssItemModel::class.java)
 
-        binding.newsRecyclerView.layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+        binding.newsRecyclerView.layoutManager = LinearLayoutManager(
+            view.context,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
         val adapter = RssItemAdapter()
         binding.newsRecyclerView.adapter = adapter
 
